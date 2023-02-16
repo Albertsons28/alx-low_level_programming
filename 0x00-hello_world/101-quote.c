e7a3d67f93a38991620df87eb501e3633abed9e2
Albertsons28 <you@example.com>
2023-02-16 09:01:17 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+/**
+ * main -print the line of code without using man(3) printf and man(3)puts
+ *
+ * Return: 1
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
