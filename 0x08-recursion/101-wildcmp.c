eda06316911ca6d0f5725b80fb2599c0e874d06a
Albertsons28 <you@example.com>
2023-03-08 00:00:50 -0800

    function that compares two strings and returns 1

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *  a function that compares two strings and returns 1
+ *  if the strings can be considered identica
+ *  Return: 1 or 0 otherwise
+ */
+int compare_strings(char *s1, char *s2)
+{
+	if ((strcmp(s1, s2) == 0)
+			{
+			return (1);
+			}
+			else
+			{
+			return (0);
+			}
+}
